301a47e484a2f320db1f9b2013842e378f24953d
Eric Lippert <eric@lippert.com>
2019-02-17 11:18:23 -0800

    Fix typo

--- Probability/Episode23.cs
+++ Probability/Episode23.cs
@@ -12,9 +12,9 @@ namespace Probability
                       from b in d10
                       from c in d10
                       from d in d10
-                      let sum = a + b + c + d
-                      where sum >= 20
-                      select sum;
+                      let s = a + b + c + d
+                      where s >= 20
+                      select s;
 
             Console.WriteLine(sum.Histogram());
         }
