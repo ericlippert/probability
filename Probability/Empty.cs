7aa27a3f571588393162b8f8b3e9acf15449aa83
Eric Lippert <eric@lippert.com>
2019-03-12 10:44:50 -0700

    Update Empty.cs

--- Probability/Empty.cs
+++ Probability/Empty.cs
@@ -4,12 +4,13 @@ using System.Linq;
 
 namespace Probability
 {
-    public sealed class Empty<T> : IDiscreteDistribution<T>
+    public sealed class Empty<T> : IDiscreteDistribution<T>, IWeightedDistribution<T>
     {
         public static readonly Empty<T> Distribution = new Empty<T>();
         private Empty() { }
         public T Sample() => throw new Exception("Cannot sample from empty distribution");
         public IEnumerable<T> Support() => Enumerable.Empty<T>();
         public int Weight(T t) => 0;
+        double IWeightedDistribution<T>.Weight(T t) => 0;
     }
 }
