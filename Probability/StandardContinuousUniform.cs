7e97bc055112db4ddc79253c977bec7be6802397
Eric Lippert <eric@lippert.com>
2019-03-12 11:00:24 -0700

    Update StandardContinuousUniform.cs

--- Probability/StandardContinuousUniform.cs
+++ Probability/StandardContinuousUniform.cs
@@ -1,11 +1,12 @@
 ﻿namespace Probability
 {
     public sealed class StandardContinuousUniform :
-        IDistribution<double>
+        IWeightedDistribution<double>
     {
         public static readonly StandardContinuousUniform
             Distribution = new StandardContinuousUniform();
         private StandardContinuousUniform() { }
         public double Sample() => Pseudorandom.NextDouble();
+        public double Weight(double x) => 0.0 <= x & x < 1.0 ? 1.0 : 0.0;
     }
 }
