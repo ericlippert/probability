40185ae29d86b719bd9d3bf6d7a5d3e1aad8b240
Eric Lippert <eric@lippert.com>
2019-03-20 16:20:35 -0700

    Create Metropolis.cs

---
+++ Probability/Metropolis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Probability
+{
+    sealed class Metropolis<T> : IWeightedDistribution<T>
+    {
+        private readonly IEnumerator<T> enumerator;
+        private readonly Func<T, double> target;
+        public static Metropolis<T> Distribution(
+            Func<T, double> target, 
+            IDistribution<T> initial, 
+            Func<T, IDistribution<T>> proposal)
+        {
+            var markov = Markov<T>.Distribution(initial, transition);
+            return new Metropolis<T>(target, markov.Sample().GetEnumerator());
+            IDistribution<T> transition(T d)
+            {
+                T candidate = proposal(d).Sample();
+                return Flip<T>.Distribution(candidate, d, target(candidate) / target(d));
+            }
+        }
+
+        private Metropolis(Func<T, double> target, IEnumerator<T> enumerator)
+        {
+            this.enumerator = enumerator;
+            this.target = target;
+        }
+
+        public T Sample()
+        {
+            this.enumerator.MoveNext();
+            return this.enumerator.Current;
+        }
+
+        public double Weight(T t) => this.target(t);
+    }
+}
+
