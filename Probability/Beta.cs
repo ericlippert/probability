4e76c2d951f1a69483dad5eb789aca7b0507beab
Eric Lippert <eric@lippert.com>
2019-03-25 17:37:45 -0400

    Add beta

---
+++ Probability/Beta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Probability
+{
+    using static System.Math;
+
+    public sealed class Beta : IWeightedDistribution<double>
+    {
+        private readonly double α; 
+        private readonly double β;
+
+        public static Beta Distribution(double alpha, double beta)
+        {
+            if (alpha <= 0.0) throw new ArgumentOutOfRangeException();
+            if (beta <= 0.0) throw new ArgumentOutOfRangeException();
+            return new Beta(alpha, beta);
+        }
+
+        private Beta(double alpha, double beta)
+        {
+            this.α = alpha;
+            this.β = beta;
+        }
+
+        public double Sample()
+        {
+            double x = Gamma.Distribution(α, 1).Sample();
+            double y = Gamma.Distribution(β, 1).Sample();
+            return x / (x + y);
+        }
+
+        // Not normalized
+        public double Weight(double x) => x <= 0.0 || x >= 1.0 ? 0.0 : Pow(x, α - 1) * Pow(1 - x, β - 1);
+
+        public override string ToString() =>
+            $"Beta({α}, {β})";
+    }
+}
--- Probability/Episode30.cs
+++ Probability/Episode30.cs
@@ -7,6 +7,10 @@ namespace Probability
         public static void DoIt()
         {
             Console.WriteLine("Episode 30 -- Continuous prior");
-       }
+
+            Console.WriteLine("Beta(5, 5) as the prior");
+            var prior = Beta.Distribution(5, 5);
+            Console.WriteLine(prior.Histogram(0, 1));
+        }
     }
 }
