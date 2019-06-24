a7772c7c01fade0337de0948e5bf96923101e7c4
Eric Lippert <eric@lippert.com>
2019-06-24 10:29:48 -0700

    More code for episode 37

--- Probability/Episode37.cs
+++ Probability/Episode37.cs
@@ -8,17 +8,28 @@ namespace Probability
         {
             Console.WriteLine("Episode 37 -- Stretching and shifting");
             ChangeTheStandardDeviation();
+            StretchIt();
         }
 
         static void ChangeTheStandardDeviation()
         {
-            // We can stretch a distribution by changing the standard deviation.
+            Console.WriteLine("We can stretch a normal distribution by changing the standard deviation.");
             var p = Normal.Distribution(0.75, 0.09);
             var p2 = Normal.Distribution(0.75, 0.15);
             double f(double x) => Atan(1000 * (x - .45)) * 20 - 31.2;
             for (int i = 0; i < 10; ++i)
                 Console.WriteLine(
                   $"{p.ExpectedValueByImportance(f, 1.0, p2):0.###}");
+        }
+
+        static void StretchIt()
+        {
+            Console.WriteLine("We can stretch an arbitrary distribution with a helper class.");
+            var p = Normal.Distribution(0.75, 0.09);
+            var ps = Stretch.Distribution(p, 2.0, 0, 0.75);
+            double f(double x) => Atan(1000 * (x - .45)) * 20 - 31.2;
+            for (int i = 0; i < 10; ++i)
+                Console.WriteLine($"{p.ExpectedValueByImportance(f, 1.0, ps):0.###}");
         }
     }
 }
---
+++ Probability/Stretch.cs
@@ -0,0 +1,25 @@
+﻿namespace Probability
+{
+    public sealed class Stretch : IWeightedDistribution<double>
+    {
+        private IWeightedDistribution<double> d;
+        private double shift;
+        private double stretch;
+
+        public static IWeightedDistribution<double> Distribution(
+            IWeightedDistribution<double> d, double stretch, double shift = 0.0, double around = 0.0)
+        {
+            if (stretch == 1.0 && shift == 0.0) return d;
+            return new Stretch(d, stretch, shift + around - around * stretch);
+        }
+        private Stretch(IWeightedDistribution<double> d, double stretch, double shift)
+        {
+            this.d = d;
+            this.stretch = stretch;
+            this.shift = shift;
+        }
+        public double Sample() => d.Sample() * stretch + shift;
+        // Dividing the weight by stretch preserves the normalization constant 
+        public double Weight(double x) => d.Weight((x - shift) / stretch) / stretch;
+    }
+}
